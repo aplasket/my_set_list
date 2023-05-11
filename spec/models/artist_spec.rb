12b6cf06183b1c7fa577392433743e797b115897
Ashley Plasket <ashley.plasket@gmail.com>
2023-05-10 21:34:03 -0400

    adds model testing, average_song_length method

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -4,4 +4,17 @@ RSpec.describe Artist, type: :model do
   describe "relationships" do
     it { should have_many :songs}
   end
+
+  describe "instance methods" do
+    describe "average song length" do
+      let!(:prince) { Artist.create!(name: "Prince") }
+      let!(:purple) { prince.songs.create!(title: "Purple Rain", length: 239, play_count: 2731) }
+      let!(:beret) { prince.songs.create!(title: "Raspberry Beret", length: 243, play_count: 1239) }
+      let!(:other_song) { prince.songs.create!(title: "Another Prince Song", length: 2, play_count: 539) }
+
+      it "returns average song length" do
+        expect(prince.average_song_length.round(2)).to eq(161.33)
+      end
+    end
+  end
 end
